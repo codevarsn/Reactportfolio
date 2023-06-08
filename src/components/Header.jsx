1d7aa5d186d3f3b5e0bdb8d03a1ff77f677c415e
codevarsn <102914988+codevarsn@users.noreply.github.com>
2023-06-08 04:43:14 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Alexander from '../img/alexander.png'
 
 function Header() {
   return (
     <header className='foto'>
-      <img className='photo' src="../icons/Alexander-Echavarria.png" alt="photo" />
+      <img className='photo' src={Alexander} alt="photo" />
     </header>
   );
 }
